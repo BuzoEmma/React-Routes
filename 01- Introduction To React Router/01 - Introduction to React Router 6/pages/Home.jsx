7e740fd514161504445500a696888995397c4e84
BuzoEmma <chibuzoemmanuel55@gmai.com>
2023-05-24 12:02:00 +0100

    first commit

---
+++ 01- Introduction To React Router/01 - Introduction to React Router 6/pages/Home.jsx
@@ -0,0 +1,18 @@
+// eslint-disable-next-line no-unused-vars
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Home = () => {
+  return (
+    <div className="home-container">
+      <h1>You got the travel plans, we got the travel vans,</h1>
+      <p>
+        Add adventure to your life by joining the #vanlife movement to make your
+        perfect road trip.
+      </p>
+      <Link to={"vans"}> Find your van</Link>
+    </div>
+  );
+};
+
+export default Home;
